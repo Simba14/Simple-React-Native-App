8b43add429a6c437c8de3e453f3b7ee22cc13175
Simba14 <seanblundell@gmail.com>
2017-05-22 21:27:57 +0100

    Add tab structure to index

--- index.ios.js
+++ index.ios.js
@@ -3,16 +3,37 @@ import React, { Component } from 'react';
 import { Navigator } from 'react-native-deprecated-custom-components'
 import {
   AppRegistry,
-  StyleSheet
+  StyleSheet,
+  TabBarIOS
 } from 'react-native';
-
 import AppNavigator from './app/navigations/AppNavigator'
 
 export default class Scratch extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedTab: "tab1"
+    }
+  }
   render() {
     return (
-      <AppNavigator />
+      <TabBarIOS
+        selectedTab={this.state.selectedTab}>
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === "tab1"}
+          title={`TAB 1`}>
+          <AppNavigator
+          initialRoute={{ident: "PeopleIndex"}} />
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === "tab2"}
+          title={`TAB 2`}>
+          <AppNavigator
+            initialRoute={{ident: "PersonShow",
+                           person: {firstName: "jordan", lastName: "leigh", roomNumber: 30}}} />
+        </TabBarIOS.Item>
+      </TabBarIOS>
     );
   }
  }
