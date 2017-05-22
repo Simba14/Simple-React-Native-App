c6ae05a4e8a68aad1c549ba298655e01d78d8faf
Simba14 <seanblundell@gmail.com>
2017-05-22 21:10:53 +0100

    Abstract navigator out of index and into own file

---
+++ app/navigations/AppNavigator.js
@@ -0,0 +1,55 @@
+'use strict'
+import React, { Component } from 'react';
+import { Navigator } from 'react-native-deprecated-custom-components'
+import {
+  StyleSheet
+} from 'react-native';
+
+import PeopleIndexScreen from '../screens/PeopleIndexScreen'
+import PersonShowScreen from '../screens/PersonShowScreen'
+
+export default class AppNavigator extends Component {
+
+  _renderScene(route, navigator) {
+    var globalNavigatorProps = { navigator } //allows global props to be injected into every screen
+
+    switch(route.ident) { //selects the screen based on the route.ident passed in
+      case "PeopleIndex":
+        return (
+          <PeopleIndexScreen
+            {...globalNavigatorProps} />
+        );
+      case "PersonShow":
+        return (
+          <PersonShowScreen
+            {...globalNavigatorProps}
+            person={route.person} />
+        )
+      default:
+        return (
+        <PeopleIndexScreen
+          {...globalNavigatorProps} />
+        );
+    }
+  }
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{ident: "PeopleIndex"}}
+        ref="appNavigator"
+        style={styles.navigatorStyles}
+        renderScene={this._renderScene}
+        />
+    );
+  }
+ }
+
+const styles = StyleSheet.create({
+
+  navigatorStyles: {
+
+  }
+});
+
+module.exports = AppNavigator
--- index.ios.js
+++ index.ios.js
@@ -6,51 +6,19 @@ import {
   StyleSheet
 } from 'react-native';
 
-import PeopleIndexScreen from './app/screens/PeopleIndexScreen'
-import PersonShowScreen from './app/screens/PersonShowScreen'
+import AppNavigator from './app/navigations/AppNavigator'
 
 export default class Scratch extends Component {
 
-  _renderScene(route, navigator) {
-    var globalNavigatorProps = { navigator } //allows global props to be injected into every screen
-
-    switch(route.ident) { //selects the screen based on the route.ident passed in
-      case "PeopleIndex":
-        return (
-          <PeopleIndexScreen
-            {...globalNavigatorProps} />
-        );
-      case "PersonShow":
-        return (
-          <PersonShowScreen
-            {...globalNavigatorProps}
-            person={route.person} />
-        )
-      default:
-        return (
-        <PeopleIndexScreen
-          {...globalNavigatorProps} />
-        );
-    }
-  }
-
   render() {
     return (
-      <Navigator
-        initialRoute={{ident: "PeopleIndex"}}
-        ref="appNavigator"
-        style={styles.navigatorStyles}
-        renderScene={this._renderScene} 
-        />
+      <AppNavigator />
     );
   }
  }
 
 const styles = StyleSheet.create({
 
-  navigatorStyles: {
-
-  }
 });
 
 AppRegistry.registerComponent('Scratch', () => Scratch);
